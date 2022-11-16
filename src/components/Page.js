080cc5b6ba1ed27a21274c9e34732fb3d614f98e
Sakina Ibrahimova <101709498+Sakneb@users.noreply.github.com>
2022-11-16 15:52:06 +0200

    Update Page.js

--- src/components/Page.js
+++ src/components/Page.js
@@ -6,7 +6,7 @@ function Page() {
       <div>
         <h2 className="about--text">About</h2>
         <p className="first--p">
-          I am a frontend developer with a particular interest in making things
+          I am a software engineer with a particular interest in making things
           simple and automating daily tasks. I try to keep up with security and
           best practices, and am always looking for new things to learn.
         </p>
